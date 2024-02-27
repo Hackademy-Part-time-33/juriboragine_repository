f4a2015c20e5ef5966a71ac3a6c09c478b554b4f
juriboragine <juri25@outlook.it>
2024-02-27 21:29:21 +0100

    juriboraginerepositoryjs

---
+++ js/main.js
@@ -0,0 +1,132 @@
+// traccia n 1 
+
+let a = 1;
+console.log(a);
+let b  = 1;
+console.log(b);
+let c = 1;
+console.log(c);
+let d = 1;
+console.log(d);
+let e = 1;
+console.log(e);
+let f = a + b + c + d + e;
+console.log(f);
+let g = f / 5;
+console.log(g);
+
+// traccia n 2 
+
+let age1 = 28;
+console.log(age1);
+let age2 = 72;
+console.log(age2);
+let age100 = age1 + age2;
+console.log(age100);
+let x = 2024;
+console.log(x);
+let y = 1995;
+console.log(y);
+let z = x - y;
+console.log(z);
+let text ='Hai';
+console.log(text)
+let text2=' anni e ti mancano';
+console.log(text2);
+let text3='anni per compierne 100';
+console.log(text3);
+let result = text1 + age1 + text2 + age2 + text3 ;
+console.log(result);
+
+//traccia n 3
+
+  let n1= parseInt(prompt("inserisci un numero"));
+  let n2= parseInt(prompt("inserisci un secondo numero"));
+  let addizione= n1+n2;
+  console.log(`l'addizione è : $(addizione) ` );
+  let sottrazione= n1-n2;
+  console.log(`la sottrazione è : $(sottrazione)`);
+  let moltiplicazione= n1*n2;
+  console.log(`la moltiplicazione è: $(moltiplicazione)`);
+  let divisione= n1/n2;
+  console.log(`la divisione è : $(divisione)`);
+  let potenza= n1**n2;
+  console.log(`la potenza è: $(potenza)`);
+  let text4="i numeri da te scelti sono";
+  let text5="i risultati delle varie operzioni sono: ";
+  console.log(text5);
+  let stamp=text4+n1+`,`+ n2+text4+`addizione`+ addizione +`sottrazioni`+sottrazioni+`moltiplicazione`+ moltiplicazione+ `divisione`+ divisione+ `potenza`+ potenza`.`;
+  console.log(stamp);
+  
+  //traccia 4
+
+  let array_1 = [
+    ['un', 'per', 'incatenarli.'],
+    ['Anello', 'trovarli,'],
+    ['ghermirli', 'e'],
+    ['gondor', 'mark'],
+  ];
+  
+  console.log(array_1[0][0],arra);
+
+  let array_2 = [
+    [['trovarli,']],
+    ['tu,', 'sciocchi'],
+    ['tu,', 'sciocchi', ['padron', 'Sauron']],
+    ['nel', ['fuggite', 'gandalf']],
+    [['domarli,', 'passare'], 'buio']
+  ];
+  console.log(array_1[0][0], array_1[1][0], array_1[0][1], array_2[4][0][0], array_1[0][0], array_1[1][0], array_1[0][1], array_2[0][0][0], array_1[0][0], array_1[1][0], array_1[0][1], array_1[2][0], array_1[2][1], array_2[3][0], array_2[4][1], array_1[0][2] );
+
+  let arrayVuoto=[]
+  arrayVuoto.push("Un'Anello per domarli", " un anello per trovarli "," un anello per ghermirli e nel buio incatenarli");
+  console.log(arrayVuoto);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
